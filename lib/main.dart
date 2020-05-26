91f54b679612a07a0aa44f231f39f148fecc9982
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-05-25 22:31:50 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:expenses/components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              child: Text('Grafico'),
+              elevation: 5,
+            ),
+          ),
+          TransactionUser(),
+        ],
+      ),
+    );
+  }
+}
