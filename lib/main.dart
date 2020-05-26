b326377f2fdcd4a712d2f5e48204183c4c74fc6e
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-05-26 00:10:26 -0300

    primarySwatch and accentColor

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,13 @@ main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        accentColor: Colors.amber
+      )
+    );
   }
 }
 
