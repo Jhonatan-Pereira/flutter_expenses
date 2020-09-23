871a18e223cf0cd2cb4802e2853a853fcf6988b1
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-09-23 20:24:46 -0300

    Construtores e Widgets Constantes

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -40,7 +40,7 @@ class TransactionList extends StatelessWidget {
               final tr = transactions[index];
               return Card(
                 elevation: 5,
-                margin: EdgeInsets.symmetric(
+                margin: const EdgeInsets.symmetric(
                   vertical: 8,
                   horizontal: 5,
                 ),
@@ -68,12 +68,12 @@ class TransactionList extends StatelessWidget {
                   trailing: MediaQuery.of(context).size.width > 400
                       ? FlatButton.icon(
                           onPressed: () => onRemove(tr.id),
-                          icon: Icon(Icons.delete),
-                          label: Text('Excluir'),
+                          icon: const Icon(Icons.delete),
+                          label: const Text('Excluir'),
                           textColor: Theme.of(context).errorColor,
                         )
                       : IconButton(
-                          icon: Icon(Icons.delete),
+                          icon: const Icon(Icons.delete),
                           color: Theme.of(context).errorColor,
                           onPressed: () => onRemove(tr.id),
                         ),
